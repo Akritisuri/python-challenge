0dcc5338718063f327bce42607ed4b10dae1293a
Akriti Suri <akriti@13-MACBOOKPRO-AKRITI.local>
2020-09-09 15:23:50 -0700

    second

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,10 +2,30 @@ import os
 import csv
 
 # Define all the variables
-total_votes = 0
+votes = []
 candidates = []
-votes_won = [0, 0, 0, 0]
-percent_won = []
+county = []
+Khan = []
+Correy = []
+Li = []
+Otooley = []
 
 #Enter path to collect data from the Recources folder
 election_csv = os.path.join("Python-challenge\Resources", 'election_data.csv')
+
+# Open and read the csv file
+with open(election_csv, 'r') as csvfile:
+
+    # Split up the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    next(csvreader)
+    
+    #Loop through the data
+    for row in csvreader:
+        votes.append(int(row[0]))
+        county.append(row[1])
+        candidates.append(row[2])
+    
+#Calculate the total number of votes
+        total_votes = (len(votes))
