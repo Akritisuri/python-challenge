e87476b4c16b5b09745703feee95ac330c73fb78
Akriti Suri <akriti@13-MACBOOKPRO-AKRITI.local>
2020-09-09 08:54:11 -0700

    fifth

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,9 @@ with open(Budget_csv, newline="") as csvfile:
     # Skip the header row
     csv_header = next(csvfile)
     print(f"Header: {csv_header}")
+    
+    # Find the total number of months
+    length_months = len(months)
 
 # Append the values to the correct rows for months and the total profit
 total_months = []
@@ -21,15 +24,25 @@ for rows in csvreader:
     total_profit.Append(int(row[1]))
 
 # Find the change in revenue
-revenue change = []
+revenue_change = []
 
 for x in range(1, len(total_profit)):
     revenue_change.Append((int(total_profit[x]) - int(total_profit[x-1])))
-
-# calculate the average change in revenue
+    
+# Calculate the average
     revenue_average = []
+    revenue_average = sum(revenue_change) / len(revenue)
+    
+#Calculate the greatest increase in profits over the entire period
+    greatest_increase = max(revenue_change)
+# Calculate the greatest decrease in profits over the entire period
+    greatest_decrease = min(revenue_change)
+
 # Print the analysis
     print("Financial Analysis")
     print("..................................")
     print("Total months: " + str(total_months))
     print("Total: " + "$" + str(sum(total_profit)))
+    print("Average change: " + "$" + str(revenue_average))
+    print("Greatest increase in profits: " + "str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
+    print("Greatest decrease in profits: " + "str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
